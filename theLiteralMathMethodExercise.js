420c5b3c444db58561954c63cc4bda2861087b17
Austin Benedict <astone@Austins-MBP.attlocal.net>
2020-10-14 13:08:28 -0500

    Completed Literal Math Method Exercise

---
+++ theLiteralMathMethodExercise.js
@@ -0,0 +1,51 @@
+let warmHugs = "Hi, I'm Olaf and I like warm hugs.";
+console.log(warmHugs.toUpperCase());
+console.log(warmHugs.replace("like", "love"));
+
+let beenImpaled = "Oh, look at that. I've been impaled.";
+console.log(beenImpaled.slice(18, 36));
+
+let dotDotDot = "...";
+let skullBones = `I don't have a skull${dotDotDot}or bones.`
+console.log(skullBones);
+
+
+console.log(Math.PI);
+
+let randomNumber = Math.random();
+randomNumber *= 3;
+randomNumber++;
+randomNumber = Math.floor(randomNumber);
+console.log(randomNumber);
+
+// OR
+
+let otherRandomNumber = Math.random();
+otherRandomNumber *= 3;
+otherRandomNumber = Math.ceil(otherRandomNumber);
+console.log(otherRandomNumber);
+
+// Bonus
+console.log("Let It Go! ".toUpperCase().repeat(3));
+
+let turtleButts = "Bright side? Turtles can breathe through their butts.";
+console.log(turtleButts.replace(/ /g, '_'));
+
+console.log(Math.sqrt(2));
+
+
+newRandomNumber = Math.floor(Math.random() * 23 + 7);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
